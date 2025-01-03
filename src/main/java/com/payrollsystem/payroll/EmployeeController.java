46cff3af7690a53aff3a6599382445c730f382e5
Mohamed Al-maary <m.almaary@outlook.com>
2025-01-03 11:04:48 +0000

    Remove double forward slashes
    This is not common to have in code that has already been wrapped. This is how it is done in the spring boot docs and not entirely sure why they have it as that in their example docs.

--- src/main/java/com/payrollsystem/payroll/EmployeeController.java
+++ src/main/java/com/payrollsystem/payroll/EmployeeController.java
@@ -33,8 +33,8 @@ class EmployeeController {
     @GetMapping("/employees")
     CollectionModel<EntityModel<Employee>> all() {
 
-        List<EntityModel<Employee>> employees = repository.findAll().stream() //
-                .map(assembler::toModel) //
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(assembler::toModel)
                 .collect(Collectors.toList());
 
         return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
@@ -45,17 +45,16 @@ class EmployeeController {
 
         EntityModel<Employee> entityModel = assembler.toModel(repository.save(newEmployee));
 
-        return ResponseEntity //
-                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
                 .body(entityModel);
     }
 
-    // Single item
 
     @GetMapping("/employees/{id}")
     EntityModel<Employee> one(@PathVariable Long id) {
 
-        Employee employee = repository.findById(id) //
+        Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
 
         return assembler.toModel(employee);
@@ -64,20 +63,20 @@ class EmployeeController {
     @PutMapping("/employees/{id}")
     ResponseEntity<?> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
 
-        Employee updatedEmployee = repository.findById(id) //
+        Employee updatedEmployee = repository.findById(id)
                 .map(employee -> {
                     employee.setName(newEmployee.getName());
                     employee.setRole(newEmployee.getRole());
                     return repository.save(employee);
-                }) //
+                })
                 .orElseGet(() -> {
                     return repository.save(newEmployee);
                 });
 
         EntityModel<Employee> entityModel = assembler.toModel(updatedEmployee);
 
-        return ResponseEntity //
-                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
                 .body(entityModel);
     }
 
